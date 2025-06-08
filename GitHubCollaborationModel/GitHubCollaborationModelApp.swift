23f94a0dc565d5141e5388907b5bfa09224605fd
Valerie Hammer <vkellogg@wisc.edu>
2025-06-08 18:08:42 -0500

    Initial Commit

---
+++ GitHubCollaborationModel/GitHubCollaborationModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationModelApp.swift
+//  GitHubCollaborationModel
+//
+//  Created by Valerie Hammer on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
